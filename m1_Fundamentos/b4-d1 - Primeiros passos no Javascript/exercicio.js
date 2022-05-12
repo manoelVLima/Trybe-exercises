43ad0833d3ef74f11b30ed438a22919fcd012039
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-05-12 09:41:21 -0300

    Iniciei o dia 4.1

---
+++ m1_Fundamentos/b4-d1 - Primeiros passos no Javascript/exercicio.js
@@ -0,0 +1,13 @@
+
+// Programa 1:
+// const a = 3;
+// const b = 3;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+// Programa 2:
---
+++ m1_Fundamentos/b4-d1 - Primeiros passos no Javascript/operadores-logicos.js
@@ -0,0 +1,38 @@
+// const comida = 'pão na chapa';
+// const bebida = 'cafezinho';
+
+// if (bebida === 'cafezinho' && comida === 'pão na chapa') {
+//   console.log('Muito obrigado pela refeição :)');
+// } else {
+//   console.log('Acho que houve um engano com meu pedido');
+// }
+
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
+
+// const currentHour = 12;
+// let message;
+
+// if(currentHour >= 22){
+//     message = "Não deveriamos comer nada, é hora de dormir";
+// } else if(currentHour >= 18 && currentHour < 22){
+//     message = "Rango da noite, vamos jantar!";
+// } else if(currentHour >= 14 && currentHour < 18) {
+//     message = "Vamos fazer um bolo pro café da tarde?"
+// } else if(currentHour >= 11 && currentHour < 14){
+//     message = "Hora do almoço!";
+// } else if(currentHour >= 4 && currentHour < 11){
+//     message = "humm,cheiro de café recém passado.";
+// }
+// console.log(message);
+
+const nota = 80;
+let message;
+switch(nota){
+    case 80:
+        message = "Aprovada"
+        console.log(message);
+    break;
+}
