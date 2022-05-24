ac3cb8a430beb1cb07e8518f9fc1632f978a870b
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-05-24 18:40:59 -0300

    Continuação dia 5.3

--- m1_Fundamentos/b5-d3 - Eventos com DOM/Aula/main.js
+++ m1_Fundamentos/b5-d3 - Eventos com DOM/Aula/main.js
@@ -26,12 +26,49 @@ thirdLi.addEventListener("click",clickClass)
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+function textBox(event){
+  let classTech = document.querySelector('.tech')
+  
+  classTech.innerHTML = event.target.value;
+  
+}
+input.addEventListener('input',textBox);
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+  function mainPage(event){
+    
+     window.open('https://www.linkedin.com/in/manoel-vieira-lima-junior-589838127/','_blank')
+  }
+  function cursor(){
+    const logo = document.querySelector('#my-spotrybefy');
+    logo.style.cursor = 'pointer';
+  }
+  myWebpage.addEventListener('dblclick',mainPage)
+  myWebpage.addEventListener('mouseover',cursor) 
+
+  //   window.location.href returns the href (URL) of the current page
+  //   window.location.hostname returns the domain name of the web host
+  //   window.location.pathname returns the path and filename of the current page
+  //   window.location.protocol returns the web protocol used (http: or https:)
+  //   window.location.assign() loads a new document 
+  // 
+ 
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+function mouseonTop(){
+  const top3 = document.querySelector('#my-spotrybefy')
+  top3.style.color = 'red';
+}
+function mouseOut(){
+  const top3 = document.querySelector('#my-spotrybefy')
+  top3.style.color = 'white'
+}
+  myWebpage.addEventListener('mouseover',mouseonTop);
+  myWebpage.addEventListener('mouseout',mouseOut)
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
